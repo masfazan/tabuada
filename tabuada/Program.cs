d7534f5c41a0cdaedabe62f0ce1b51ee87012a9b
adm <msfazan@icloud.com>
2024-04-18 15:42:22 -0300

    Add project files.

---
+++ tabuada/Program.cs
@@ -0,0 +1,10 @@
+﻿int n1 = 0, tabuada = 0;
+
+Console.WriteLine("Digite o numero: ");
+n1=int.Parse(Console.ReadLine());
+
+for (int i = 0; i<11 && n1>0; i++)
+{
+    tabuada = n1 * i;
+    Console.WriteLine(n1+"x"+i+"="+tabuada);
+}
